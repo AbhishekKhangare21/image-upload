cf925aa24cb45e63f5794fc4ff435efdeb90226d
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-03-11 18:43:24 +0530

    Added UUID dependency and updated multer to support custom file names and file type filtering.

--- app.js
+++ app.js
@@ -1,17 +1,56 @@
 const express = require("express");
 const multer = require("multer");
-
-const PORT = 4000;
+const uuid = require("uuid").v4;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ hello: "abhishek" });
+//middleware
+const upload = multer({ dest: "uploads/" });
+
+// for single file
+// app.post("/upload", upload.single("file"), (req, res) => {
+//   res.json({ status: "success" });
+// });
+
+// for multiple files
+// app.post("/upload", upload.array("file", 2), (req, res) => {
+//   res.json({ status: "success" });
+// });
+
+// multiple fields upload
+// const multiUpload = upload.fields([
+//   { name: "avatar", maxCount: 2 },
+//   { name: "resume", maxCount: 2 },
+// ]);
+
+// app.post("/upload", multiUpload, (req, res) => {
+//   console.log(req.files);
+//   res.json({ status: "success" });
+// });
+
+// custom filename
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    const { originalname } = file;
+    cb(null, `${uuid()}-${originalname}`);
+  },
 });
 
-app.post("/upload", (req, res) => {
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.split("/")[0] === "image") {
+    cb(null, true);
+  } else {
+    cb(new Error("file is not of the correct type"), false);
+  }
+};
+
+const upload1 = multer({ storage, fileFilter });
+app.post("/upload", upload1.array("file"), (req, res) => {
   res.json({ status: "success" });
 });
 
-app.listen(PORT, () => {
-  console.log(`server is running at localhost:${PORT}`);
+app.listen(4000, () => {
+  console.log(`server is running at localhost:4000`);
 });
