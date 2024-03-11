eb128df12d7fd72ddd76a8eef1db92fb4a433145
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-03-11 16:34:24 +0530

    Added multer middleware for file uploads.

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const multer = require("multer");
 
 const PORT = 4000;
 const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ hello: "abhishek" });
+});
+
+app.post("/upload", (req, res) => {
+  res.json({ status: "success" });
+});
 
 app.listen(PORT, () => {
   console.log(`server is running at localhost:${PORT}`);
