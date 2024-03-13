a82d994f4c4c82fa2da7857308705cdc375d34af
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-03-13 11:32:23 +0530

    Update AWS credentials and use the new credentials to get a signed URL for an object in S3

---
+++ app.js
@@ -0,0 +1,25 @@
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+
+const s3Client = new S3Client({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: "AKIATCKANTVK7YZJ62VA",
+    secretAccessKey: "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",
+  },
+});
+
+async function getObjectURL(key) {
+  const command = new GetObjectCommand({
+    Bucket: "bucket.piyush.private",
+    Key: key,
+  });
+  const url = await getSignedUrl(s3Client, command);
+  return url;
+}
+
+async function init() {
+  console.log("URL for boy.jpg", await getObjectURL("boy.jpg"));
+}
+
+init();
