19b6ad943452df4ec66d4f6549ed153d3d0e3017
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-03-13 11:36:02 +0530

    Update AWS credentials to static values

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const s3Client = new S3Client({
   region: "ap-south-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: "AKIATCKANTVK7YZJ62VA",
+    secretAccessKey: "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",
   },
 });
 
