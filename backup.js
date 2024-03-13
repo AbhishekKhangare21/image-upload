975923923a6727fb9bd0274cece34751ef32c1cd
Abhishek Khangare <abhishekkhangare21@gmail.com>
2024-03-13 18:15:23 +0530

    Update AWS S3 client credentials and add functions for uploading objects and listing objects

--- backup.js
+++ backup.js
@@ -10,8 +10,8 @@ const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const s3Client = new S3Client({
   region: "ap-south-1",
   credentials: {
-    accessKeyId: "AKIATCKANTVK5QDC6KK6",
-    secretAccessKey: "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",
+    accessKeyId: "AKIATCKANTVK7YZJ62VA",
+    secretAccessKey: "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",
   },
 });
 
@@ -34,29 +34,40 @@ async function putObject(filename, contentType) {
   return url;
 }
 
+async function listObjects() {
+  const command = new ListObjectsV2Command({
+    Bucket: "bucket.piyush.private",
+    key: "/",
+  });
+
+  const result = await s3Client.send(command);
+  console.log("result ==>", result);
+}
+
 async function init() {
   // await listObjects();
-  //   console.log(
-  //     "URL for boy.jpg",
-  //     await getObjectURL("uploads/user-uploads/video-1710333037567.mp4")
-  //   );
+  // console.log(
+  //   "URL for boy.jpg",
+  //   await getObjectURL("/uploads/user-uploads/image-1710318429273.jpeg")
+  // );
   // console.log(
   //   "URL for boy.jpg",
   //   await getObjectURL("uploads/user-uploads/video-1710319338135.mp4")
   // );
   // console.log(
-  //   "URL for uploading",
+  //   "URL form uploading",
   //   await putObject(`image-${Date.now()}.jpeg`, `images/jpeg`)
   // );
-  //   console.log(
-  //     "URL for uploading",
-  //     await putObject(`video-${Date.now()}.mp4`, `video/mp4`)
-  //   );
-  //   const cmd = new DeleteObjectCommand({
-  //     Bucket: "bucket.piyush.private",
-  //     Key: "49.webp",
-  //   });
-  //   await s3Client.send(cmd);
+  // console.log(
+  //   "URL form uploading",
+  //   await putObject(`video-${Date.now()}.mp4`, `video/mp4`)
+  // );
+
+  const cmd = new DeleteObjectCommand({
+    Bucket: "bucket.piyush.private",
+    Key: "boy.jpg",
+  });
+  await s3Client.send(cmd);
 }
 
 init();
